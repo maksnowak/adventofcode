395c41775336b5eb8855838e3ee2579f0f50e93d
maksnowak <59841779+maksnowak@users.noreply.github.com>
2021-12-04 20:17:47 +0100

    day 4

---
+++ 2021/4/1.py
@@ -0,0 +1,34 @@
+data = open('2021/4/input.txt', 'r').read().split()
+nums = data[0].split(',')
+_boards = data[1:]
+winning = 0
+_sum = 0
+boards = [[] for _ in range(100)]
+i = 0
+for num in _boards:
+    boards[i].append(num)
+    if len(boards[i]) == 25:
+        i += 1
+for num in nums:
+    for i, board in enumerate(boards):
+        if num in board:
+            board[board.index(num)] = "*"
+        for j in range(0, 20, 5):
+            if board[j] == board[j + 1] == board[j + 2] == board[j + 3] == board[j + 4]:
+                winnning = i
+                break
+        for k in range(0, 5):
+            if board[k] == board[k + 5] == board[k + 10] == board[k + 15] == board[k + 20]:
+                winning = i
+                break
+        if winning != 0:
+            break
+    if winning != 0:
+        last = int(num)
+        break
+for num in boards[winning]:
+    try:
+        _sum += int(num)
+    except:
+        pass
+print(_sum * last)
