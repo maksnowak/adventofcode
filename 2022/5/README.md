72c5e9677757859aa4d29703f9806047b8e248a9
maksnowak <59841779+maksnowak@users.noreply.github.com>
2022-12-06 15:08:54 +0100

    Fix year 2022 day 5 (#3)

--- 2022/5/README.md
+++
@@ -1,7 +0,0 @@
-I modified the program's input, so the stack view looks like this:
-```
-[Z][N]
-[M][C][D]
-[P]
-```
-I will fix it if I find a way how to do it with a standard input lol
--- 2022/5/main.py
+++ 2022/5/main.py
@@ -1,42 +1,57 @@
 #TODO make the program work with a standard input, not a modified one (see README.md)
 def firstPuzzle(file):
-    data = open(file).read().strip().split('\n\n')
-    crates = data[0].split('\n')
-    instructions = data[1]
-    crates_list = []
-    for stack in crates:
-        stripped = stack[1:-1]
-        crates_list.append(stripped.split(']['))
-    for instruction in instructions.split('\n'):
+    data = open(file).read().split('\n\n')
+    crates = data[0]
+    instructions = data[1].strip().split('\n')
+    crates_list = crates.split('\n')
+    final_list = [list(line[1::4]) for line in crates_list[:-1]]
+    stacks = []
+    for i in range(len(final_list[0])):
+        stack = []
+        for j in range(len(final_list)):
+            try:
+                if final_list[-1-j][i] != ' ':
+                    stack.append(final_list[-1-j][i])
+            except:
+                pass
+        stacks.append(stack)
+    for instruction in instructions:
         instruction_list = instruction.split(' ')
-        to_be_moved = crates_list[int(instruction_list[3]) - 1][int(instruction_list[1])*(-1):]
+        to_be_moved = stacks[int(instruction_list[3]) - 1][int(instruction_list[1])*(-1):]
         for _ in range(int(instruction_list[1])):
-            crates_list[int(instruction_list[3]) - 1].pop()
+            stacks[int(instruction_list[3]) - 1].pop()
         for item in to_be_moved[-1::-1]:
-            crates_list[int(instruction_list[5]) - 1].append(item)
+            stacks[int(instruction_list[5]) - 1].append(item)
     string = ""
-    for stack in crates_list:
+    for stack in stacks:
         string += stack[-1]
     return string
 
-
 def secondPuzzle(file):
-    data = open(file).read().strip().split('\n\n')
-    crates = data[0].split('\n')
-    instructions = data[1]
-    crates_list = []
-    for stack in crates:
-        stripped = stack[1:-1]
-        crates_list.append(stripped.split(']['))
-    for instruction in instructions.split('\n'):
+    data = open(file).read().split('\n\n')
+    crates = data[0]
+    instructions = data[1].strip().split('\n')
+    crates_list = crates.split('\n')
+    final_list = [list(line[1::4]) for line in crates_list[:-1]]
+    stacks = []
+    for i in range(len(final_list[0])):
+        stack = []
+        for j in range(len(final_list)):
+            try:
+                if final_list[-1-j][i] != ' ':
+                    stack.append(final_list[-1-j][i])
+            except:
+                pass
+        stacks.append(stack)
+    for instruction in instructions:
         instruction_list = instruction.split(' ')
-        to_be_moved = crates_list[int(instruction_list[3]) - 1][int(instruction_list[1])*(-1):]
+        to_be_moved = stacks[int(instruction_list[3]) - 1][int(instruction_list[1])*(-1):]
         for _ in range(int(instruction_list[1])):
-            crates_list[int(instruction_list[3]) - 1].pop()
+            stacks[int(instruction_list[3]) - 1].pop()
         for item in to_be_moved:
-            crates_list[int(instruction_list[5]) - 1].append(item)
+            stacks[int(instruction_list[5]) - 1].append(item)
     string = ""
-    for stack in crates_list:
+    for stack in stacks:
         string += stack[-1]
     return string
 
